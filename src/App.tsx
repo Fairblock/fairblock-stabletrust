4dba99393f1b970a8a6cd55fe3da6dac9152e96d
pememoni <34263018+pememoni@users.noreply.github.com>
2026-08-23 13:01:12 -0400

    Streamline confidentiality story sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import Section01 from "./components/Section01";
 import UniversalApp from "./components/UniversalApp";
 import ProductFamily from "./components/ProductFamily";
-import Section02 from "./components/Section02";
 import Section03 from "./components/Section03";
 import Section04 from "./components/Section04";
 import Section05 from "./components/Section05";
@@ -20,7 +19,6 @@ function App() {
         <Section01 />
         <UniversalApp />
         <ProductFamily />
-        <Section02 />
         <Section03 />
         <Section04 />
         <Section05 />
